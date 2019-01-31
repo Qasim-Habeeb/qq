9c7fc39e471fc442a96a120845386a1903a57333
Qasim-Habeeb <arsto1982@yahoo.com>
2019-01-31 12:10:54 +0300

    jj

---
+++ src/com/muhammedessa/muhammedmethods/Methods_agrs2.java
@@ -0,0 +1,19 @@
+package com.muhammedessa.muhammedmethods;
+
+ 
+
+public class Methods_agrs2 {
+
+	public static void main(String[] args) {
+		
+	 
+		 Muham("Muhammed Essa");
+
+	}
+	
+	 static void Muham(String t){
+		System.out.println(t);
+	}
+	
+
+}
---
+++ src/com/muhammedessa/muhammedmethods/Methods_agrs3.java
@@ -0,0 +1,32 @@
+package com.muhammedessa.muhammedmethods;
+
+ 
+public class Methods_agrs3 {
+
+	public static void main(String[] args) {
+
+		 Muham("Muhammed Essa","AL- Timimy");
+		 number(27,28);
+		 numberString("Ali", 2000);
+         String n = "Hello Java";
+         Muhamm(n);
+          
+	}
+
+	 
+		static void Muham(String t ,String b){
+			System.out.println(t + " " +b);
+		}
+		static void number(int age,int ag){
+			System.out.println(age +ag);
+		}
+		static void numberString(String name,int salary){
+			System.out.println(name +" , "+salary+"$");
+		}
+		static void Muhamm(String r){
+			System.out.println(r );
+		}
+		
+		 
+
+}
